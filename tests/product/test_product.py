d62e262bd81562336e016bf2a44bbe924dfe363f
Carlos Augusto <caarlos.san@gmail.com>
2023-04-04 18:23:29 -0300

    Add test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+import pytest
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def test_cria_produto(product):
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+    assert test_cria_produto(product) == {
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    }
